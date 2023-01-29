87b9b24419b5c2f899a6a7a547c46e0d0ec14154
Dilermando Queiroz <dilermando.queiroz@aluno.ufabc.edu.br>
2023-01-29 19:47:01 -0300

    update

--- speleothem/edge_detector/generate_data.py
+++ speleothem/edge_detector/generate_data.py
@@ -1,16 +1,11 @@
 import cv2
 
-img = cv2.imread("../image/lola/speleothem-lola-big.tif")
+img = cv2.imread("../image/dbe/DBE_4_1600_cut.tif")
 x_axis = (img.shape[0]//256)*256
 y_axis = (img.shape[1]//256)*256
 
-tiles = []
-
+i=0
 for x in range(0, x_axis, 256):
     for y in range(0, y_axis, 256):
-        tiles.append(img[x:x+256, y:y+256])
-
-i = 0
-for image in tiles:
-    cv2.imwrite(f"../image/lola/classify/{i}.png", image)
-    i += 1
+        cv2.imwrite(f"../image/dbe/classify_1600/{i}.png", img[x:x+256, y:y+256])
+        i += 1
