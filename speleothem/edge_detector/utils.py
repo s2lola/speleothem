28dc1d8b959fe873a92c44a9f3849267864014b3
Dilermando Queiroz <dilermando.queiroz@aluno.ufabc.edu.br>
2023-01-30 18:49:37 -0300

    update

--- speleothem/edge_detector/utils.py
+++ speleothem/edge_detector/utils.py
@@ -2,6 +2,7 @@ import cv2
 import numpy as np
 import pandas as pd
 from sklearn.metrics import mean_absolute_error
+from scipy.signal import find_peaks
 
 def serch_best_parameters(image_file: str, real_value):
     img = cv2.imread(image_file)
@@ -36,29 +37,46 @@ def count_edge(img):
 
     return np.mean(counts), np.std(counts)
 
-def count_canny(file, min, max):
+def count_canny(file, min, max, center=False):
     img = cv2.imread(file)
     img = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
     img = cv2.GaussianBlur(img, (3, 3), 3, 3)
+    
+    if center:
+        img = img[:, img.shape[1]//2: img.shape[1]//2 + 1]
 
     img_canny = cv2.Canny(img, min, max)
     contagem, std = count_edge(img_canny)
 
     return contagem
 
-def search_best_parameters_database(database_dir, image_dir):
+def count_find_peaks(file):
+    img = cv2.imread(file)
+    img = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+    img = img[:, img.shape[1]//2: img.shape[1]//2 + 1]
+    img = img.reshape((img.shape[0],))
+
+    peaks = find_peaks(img)
+    count = len(peaks[0])/2
+
+    return count
+
+def search_best_parameters_database(database_dir, image_dir, center=False):
     df = pd.read_csv(database_dir)
 
     def count_method(x):
         file = f"{image_dir}/{x}.png"
-        return count_canny(file, min, max)
+        return count_canny(file, min, max, center)
     
     error_aux = float("inf")
     
     values = [0, 0]
 
-    for max in range(10, 25):
-        for min in range(10, max):
+    a = 10
+    b = 25
+
+    for max in range(a, b):
+        for min in range(a, max):
             df["count_method"] = df.file.apply(count_method)
 
             error = mean_absolute_error(df["count"].to_numpy(), df["count_method"].to_numpy())
@@ -69,4 +87,3 @@ def search_best_parameters_database(database_dir, image_dir):
                 values[1] = max
 
     return values[0], values[1], error_aux
-
