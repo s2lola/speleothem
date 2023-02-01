36d66807f58deeb2f9ce47ed9886f5e21a34236a
Lola Wenzel <loretawenzel@usp.br>
2023-01-31 21:01:35 -0300

    teste

--- speleothem/edge_detector/generate_data.py
+++ speleothem/edge_detector/generate_data.py
@@ -1,11 +1,14 @@
 import cv2
 
-img = cv2.imread("../image/dbe/DBE_4_900_cut_2.tif")
-x_axis = (img.shape[0]//256)*256
-y_axis = (img.shape[1]//256)*256
+size = 48
+file = 300
+
+img = cv2.imread(f"../image/dbe/DBE_4_{file}_cut_2.tif")
+x_axis = (img.shape[0]//size)*size
+y_axis = (img.shape[1]//size)*size
 
 i=0
-for x in range(0, x_axis, 256):
-    for y in range(0, y_axis, 256):
-        cv2.imwrite(f"../image/dbe/classify_900/{i}.png", img[x:x+256, y:y+256])
+for x in range(0, x_axis, size):
+    for y in range(0, y_axis, size):
+        cv2.imwrite(f"../image/dbe/classify_{file}/{i}.png", img[x:x+size, y:y+size])
         i += 1
